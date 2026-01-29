83d4d1e937b2dc1acb886678185678b583eed1d4
Khushal Nagpure <khushalnagpure999@gmail.com>
2026-01-29 23:37:49 +0530

    Update leetcode logic

---
+++ leetcode/Strings/rotated_string.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // Check if goal is a rotation of s using string doubling method
+    bool rotateString(string& s, string& goal) {
+        if(s.length() != goal.length())
+            return false;
+        string doubleS = s + s;
+        return doubleS.find(goal) != string :: npos;
+
+        }
+};
+
+int main() {
+    Solution sol;
+
+    string s = "rotation";
+    string goal = "tionrota";
+    cout << (sol.rotateString(s, goal) ? "true" : "false") << endl;
+
+    return 0;
+}
